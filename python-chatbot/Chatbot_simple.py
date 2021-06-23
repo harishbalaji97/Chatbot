6062b5e07d3f1388171dcab68811843d2a60015b
harishbalaji97 <harish9721@gmail.com>
2021-06-23 16:44:27 +0530

    Sample commit , remove unwanted lines

--- python-chatbot/Chatbot_simple.py
+++ python-chatbot/Chatbot_simple.py
@@ -5,11 +5,6 @@ BRAIN_FILE="brain.dump"
 
 k = aiml.Kernel()
 
-# To increase the startup speed of the bot it is
-# possible to save the parsed aiml files as a
-# dump. This code checks if a dump exists and
-# otherwise loads the aiml from the xml files
-# and saves the brain dump.
 if os.path.exists(BRAIN_FILE):
     print("Loading from brain file: " + BRAIN_FILE)
     k.loadBrain(BRAIN_FILE)
